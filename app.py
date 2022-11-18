dc4db03434262b2ddeeb470444a6e9e21a0a7ac4
Vinisha-Govind-Gomare <72209665+Vinisha-Govind-Gomare@users.noreply.github.com>
2022-11-18 15:31:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,1 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
-    return "test!!"
-
-    return "this is a test.!!!"
+print ("Hello World")
